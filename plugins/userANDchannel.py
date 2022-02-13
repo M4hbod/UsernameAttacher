a6c91e198613c948dac4ff2130ee58efa8164292
M4hbod <74229780+M4hbod@users.noreply.github.com>
2022-02-13 20:02:23 +0330

    Update userANDchannel.py

--- plugins/userANDchannel.py
+++ plugins/userANDchannel.py
@@ -1,7 +1,7 @@
 from pyrogram.client import Client
 from helpers.utils import handle_user_status
 
-@Client.on_message()
+@Client.on_message(group=-1)
 async def _(bot: Client, cmd):
     try:
         await handle_user_status(bot, cmd)
