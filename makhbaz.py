3801df933430c5bdc58150d95fdfb7eb15c280fc
M4hbod <74229780+M4hbod@users.noreply.github.com>
2022-02-13 19:36:41 +0330

    logging deleted

--- makhbaz.py
+++ makhbaz.py
@@ -1,14 +1,6 @@
 import config
-import logging
 from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
-
-
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 app = Client(
     ":memory:",
@@ -17,7 +9,6 @@ app = Client(
     bot_token=config.BOT_TOKEN,
     plugins=dict(root="plugins"),
 )
-
 
 # Run Bot
 if __name__ == "__main__":
