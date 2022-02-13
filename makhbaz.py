397bddad599b5c8acdd4a4fc331bf96c388e46cd
M4hbod <74229780+M4hbod@users.noreply.github.com>
2022-02-13 19:33:56 +0330

    Update makhbaz.py

--- makhbaz.py
+++ makhbaz.py
@@ -4,7 +4,7 @@ from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
-logging.basicconfig(
+logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
