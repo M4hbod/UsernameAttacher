52a28c653c07c04c972ef62a0233812a82bb02c7
M4hbod <74229780+M4hbod@users.noreply.github.com>
2022-02-15 14:22:23 +0330

    Update change_mode.py

--- plugins/change_mode.py
+++ plugins/change_mode.py
@@ -2,13 +2,15 @@ from pyrogram import Client, filters
 from helpers.database import db
 from asyncio import sleep
 
-@Client.on_message(filters.channel & filters.regex(r"^(set)") & ~filters.forwarded, group=7)
+@Client.on_message(filters.channel & filters.regex(r"^(set)") & ~filters.forwarded, group=-4)
 async def change_mode(c,m):
     me = await c.get_chat_member(m.chat.id, "me")
     if m.chat.username:
         if me.can_edit_messages:
             mode_text = m.text[4:].strip().lower()
             print(mode_text)
+            print(len(mode_text))
+            print(len('bold'))
             if mode_text == 'normal':
                 mode = 'normal'
             if mode_text == 'bold':
@@ -23,9 +25,10 @@ async def change_mode(c,m):
                 mode = 'spoiler'
             else:
                 await m.edit('Incorrect mode!')
+                return
             try:
                 await db.change_mode(m.chat.id, mode)
-                await m.edit(f"Mode Changed To -> {mode.upper}")
+                await m.edit(f"Mode Changed To -> {mode}")
             except Exception as e:
                 print(e)
             await sleep(5)
