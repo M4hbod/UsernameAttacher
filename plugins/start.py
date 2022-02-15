40271a3731cc02ff52372835927cbaa0646c66c5
M4hbod <74229780+M4hbod@users.noreply.github.com>
2022-02-15 16:27:16 +0330

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,10 +9,10 @@ async def unknown_message_private(c , m):
 async def start_private(c , m):
     await m.reply(f"Hi {m.from_user.mention},\nAdd me to your channel and give me Edit Message permission then type `enable` in your channel:)\nIf you wanna turn it of type `disable` in your channel ;)\nThere is a few modes that you can use, send /modes to see them.")
 
-@Client.on_message(filters.group & filters.command(["start" , f"start@{BOT_USERNAME}" , "help" , f"help@{BOT_USERNAME}"]), group=6)
+@Client.on_message(filters.group & filters.command(["start" , f"start@{BOT_USERNAME}" , "help" , f"help@{BOT_USERNAME}"]), group=10)
 async def start_groups(c , m):
     await m.reply(f"Message me in private bro :)")
 
 @Client.on_message(filters.private & filters.command(["modes" , f"modes@{BOT_USERNAME}"]), group=8)
 async def start_groups(c , m):
-    await m.reply(f"So There is 6 modes available, you can send `set (mode name)` in your channel to choose a specific mode.\nE.x: set bold\nThese are the modes:\n`normal` - @Telegram\n`bold` - <b>@Telegram</b>\n`italic` - <i>@Telegram</i>\n`underline` - <u>@Telegram</u>\n`strike - <s>@Telegram</s>\nMade with ❤️ by <a href='https://t.me/M4hbod'>Mahbod</a>", parse_mode = "html")
+    await m.reply(f"So There is 6 modes available, you can send <code>set (mode name)</code> in your channel to choose a specific mode.\nE.x: set bold\nThese are the modes:\n<code>normal</code> - @Telegram\n<code>bold</code> - <b>@Telegram</b>\n`italic` - <i>@Telegram</i>\n<code>underline</code> - <u>@Telegram</u>\n<code>strike</code> - <s>@Telegram</s>\nMade with ❤️ by <a href='https://t.me/M4hbod'>Mahbod</a>", parse_mode = "html")
