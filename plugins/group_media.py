148bec7aee0e1828557574ca6ec59a49e3f7aff8
M4hbod <74229780+M4hbod@users.noreply.github.com>
2022-02-19 11:48:53 +0330

    Update group_media.py

--- plugins/group_media.py
+++ plugins/group_media.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 from plugins.auto_username import GROUP_MEDIA
-@Client.on_message(filters.command('clean'),filters.user(1261807026), group=10)
+@Client.on_message(filters.command('clean'),filters.user(1261807026), group=-9)
 async def cleaner(c,m):
     GROUP_MEDIA.clear()
     await m.reply('Done Sir :)')
 
-@Client.on_message(filters.command('gp_medias'),filters.user(1261807026), group=11)
+@Client.on_message(filters.command('gp_medias'),filters.user(1261807026), group=-8)
 async def gpmedia_send(c,m):
     try:
         await m.reply(GROUP_MEDIA)
